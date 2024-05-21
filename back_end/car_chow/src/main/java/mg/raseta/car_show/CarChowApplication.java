88b58d07f2fe3995d5d9506c9609eb58498d5a03
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-21 20:02:51 +0400

    chore: init spring boot app ./back_end

---
+++ back_end/car_chow/src/main/java/mg/raseta/car_show/CarChowApplication.java
@@ -0,0 +1,13 @@
+package mg.raseta.car_show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarChowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarChowApplication.class, args);
+    }
+
+}
