685196481bb2133c2449dc7cf0525a2dc6abda94
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-22 13:57:41 +0400

    chore: User model

---
+++ back_end/car_chow/src/main/java/mg/raseta/car_show/model/User.java
@@ -0,0 +1,29 @@
+package mg.raseta.car_show.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
