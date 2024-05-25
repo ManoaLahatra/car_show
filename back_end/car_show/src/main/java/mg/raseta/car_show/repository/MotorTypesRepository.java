a81773d8b3884e9180cb272e405b4591ae386226
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-25 12:20:59 +0400

    chore: MotorTypesRepository::findMotorTypesByName

--- back_end/car_show/src/main/java/mg/raseta/car_show/repository/MotorTypesRepository.java
+++ back_end/car_show/src/main/java/mg/raseta/car_show/repository/MotorTypesRepository.java
@@ -1,9 +1,13 @@
 package mg.raseta.car_show.repository;
 
 import mg.raseta.car_show.model.MotorTypes;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MotorTypesRepository extends JpaRepository<MotorTypes, Integer>, CrudRepository<MotorTypes, Integer> {}
+public interface MotorTypesRepository extends JpaRepository<MotorTypes, Integer>, CrudRepository<MotorTypes, Integer> {
+    Page<MotorTypes> findMotorTypesByName(String name, Pageable pageable);
+}
