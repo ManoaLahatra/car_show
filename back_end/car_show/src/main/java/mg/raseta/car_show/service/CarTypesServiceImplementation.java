607dde5fd521838cdc28248ea6babf24993bf46b
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-25 10:41:35 +0400

    chore: CarTypesService

---
+++ back_end/car_show/src/main/java/mg/raseta/car_show/service/CarTypesServiceImplementation.java
@@ -0,0 +1,62 @@
+package mg.raseta.car_show.service;
+
+import lombok.AllArgsConstructor;
+import mg.raseta.car_show.model.CarTypes;
+import mg.raseta.car_show.repository.CarTypesRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class CarTypesServiceImplementation implements CarTypesService {
+
+    private final CarTypesRepository carTypesRepository;
+
+    @Override
+    public CarTypes createCarType(CarTypes carTypes) {
+        return carTypesRepository.save(carTypes);
+    }
+
+    @Override
+    public Page<CarTypes> findAllCarTypes(int page, int limit) {
+        Pageable pageable = PageRequest.of(page, limit);
+        return carTypesRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<CarTypes> findCarTypeById(int carTypeId) {
+        return carTypesRepository.findById(carTypeId);
+    }
+
+    @Override
+    public Page<CarTypes> findCarTypesByName(String carTypeName, int page, int limit) {
+        Pageable pageable = PageRequest.of(page, limit);
+        return carTypesRepository.findCarTypesByName(carTypeName, pageable);
+    }
+
+    @Override
+    public CarTypes updateCarType(int carTypeId, CarTypes carTypes) {
+        return carTypesRepository.findById(carTypeId)
+                .map(carType -> {
+                    carType.setCarTypeId(carTypeId);
+                    carType.setName(carTypes.getName());
+                    return carTypesRepository.save(carType);
+                })
+                .orElseThrow(() -> new RuntimeException("CarType with id : " + carTypeId + " not found."));
+    }
+
+    @Override
+    public void deleteCarType(int carTypeId) {
+        Optional<CarTypes> carTypes = carTypesRepository.findById(carTypeId);
+        if (carTypes.isPresent()) {
+            carTypesRepository.delete(carTypes.get());
+        } else {
+            throw new RuntimeException("CarType with id : " + carTypeId + " not found.");
+        }
+    }
+
+}
