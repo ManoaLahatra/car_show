a295880a2557ca0a3d0bcb51685c26b049ff5e9b
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-25 10:12:55 +0400

    chore: add findBrandByName

--- back_end/car_show/src/main/java/mg/raseta/car_show/repository/BrandRepository.java
+++ back_end/car_show/src/main/java/mg/raseta/car_show/repository/BrandRepository.java
@@ -1,9 +1,13 @@
 package mg.raseta.car_show.repository;
 
 import mg.raseta.car_show.model.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BrandRepository extends JpaRepository<Brand, Integer>, CrudRepository<Brand, Integer> {}
+public interface BrandRepository extends JpaRepository<Brand, Integer>, CrudRepository<Brand, Integer> {
+    Page<Brand> findBrandByName(String brandName, Pageable pageable);
+}
