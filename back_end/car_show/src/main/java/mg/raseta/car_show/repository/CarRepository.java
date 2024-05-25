ccd6feee52b158225ee2a0cd4e61ddb977a39041
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-25 09:15:27 +0400

    chore: add Spring Security & CarRepository

---
+++ back_end/car_show/src/main/java/mg/raseta/car_show/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package mg.raseta.car_show.repository;
+
+import mg.raseta.car_show.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Integer>, CrudRepository<Car, Integer> {}
