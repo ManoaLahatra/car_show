6b6528c70eb7a5d87b048c0dbcfa0a06f795304c
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-27 13:26:35 +0400

    chore: ImagesRepository extend GenericRepository

--- back_end/car_show/src/main/java/mg/raseta/car_show/repository/ImagesRepository.java
+++ back_end/car_show/src/main/java/mg/raseta/car_show/repository/ImagesRepository.java
@@ -1,9 +1,7 @@
 package mg.raseta.car_show.repository;
 
 import mg.raseta.car_show.model.Images;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ImagesRepository extends JpaRepository<Images, Integer>, CrudRepository<Images, Integer> {}
+public interface ImagesRepository extends GenericRepository<Images, Integer> {}
