e539f90433488d65068f0098425bfd6e2161c8f0
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-27 12:52:03 +0400

    chore: MotorTypesRepository use generic

--- back_end/car_show/src/main/java/mg/raseta/car_show/repository/MotorTypesRepository.java
+++ back_end/car_show/src/main/java/mg/raseta/car_show/repository/MotorTypesRepository.java
@@ -1,11 +1,6 @@
 package mg.raseta.car_show.repository;
 
 import mg.raseta.car_show.model.MotorTypes;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
