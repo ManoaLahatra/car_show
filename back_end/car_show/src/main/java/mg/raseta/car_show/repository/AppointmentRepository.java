2e73046fb69ad1d3ab010b625f74966050e2f35f
ManoaHEI <hei.manoa.4@gmail.com>
2024-05-27 14:59:16 +0400

    chore: AppointmentRepository extend GenericRepository

--- back_end/car_show/src/main/java/mg/raseta/car_show/repository/AppointmentRepository.java
+++ back_end/car_show/src/main/java/mg/raseta/car_show/repository/AppointmentRepository.java
@@ -1,9 +1,7 @@
 package mg.raseta.car_show.repository;
 
 import mg.raseta.car_show.model.Appointment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppointmentRepository extends JpaRepository<Appointment, Integer>, CrudRepository<Appointment, Integer> {}
+public interface AppointmentRepository extends GenericRepository<Appointment, Integer> {}
